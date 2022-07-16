d85642b2c70d525beb9c6548129c56a40bdf3e1b
AllanRanier <allanranier18@gmail.com>
2022-07-16 13:23:08 -0300

    first commit

---
+++ lib/model/cidades.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Cidades {
+  final String cep;
+  final String logradouro;
+  final String complemento;
+  final String bairro;
+  final String localidade;
+  final String uf;
+  final String ddd;
+
+  
+  Cidades({
+    required this.cep,
+    required this.logradouro,
+    required this.complemento,
+    required this.bairro,
+    required this.localidade,
+    required this.uf,
+    required this.ddd,
+  });
+
+}
